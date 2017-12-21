0371f84b7ebc61215a615e8ba38ab8c7153ab8ae
graeme doran <graeme42@icloud.com>
2017-12-21 10:39:05 +0000

    my chances

--- client/src/views/country_info.js
+++ client/src/views/country_info.js
@@ -3,7 +3,26 @@ const CountryInfo = function () {
 }
 
 CountryInfo.prototype.render = function (country) {
-  // TODO: Render country info
+  const app = function(){
+
+    const selectDropDown = document.querySelector('#dropDown');
+    requestCountryAPI.get(getCountiresRequestComplete);
+  }
+
+  const addToList = function(country){
+
+    const ul = document.querySelector('ul');
+    const liName = document.createElement('li');
+    liName.innerText = country.name;
+    const liCapital = document.createElement('li');
+    liCapital.innerText = country.capital;
+    const liFlag = document.createElement('img');
+    liFlag.src = country.flag;
+    liFlag.style.width = "50px";
+    ul.appendChild(liName);
+    ul.appendChild(liCapital);
+    ul.appendChild(liFlag);
+  }  // TODO: Render country info
 }
 
 CountryInfo.prototype.createListItem = function (label, content) {
